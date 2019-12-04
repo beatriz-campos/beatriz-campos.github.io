f40ab859d1c833b35b969a8fd7ec7a062b21eacc
bia <beatrizcrcm@gmail.com>
2019-12-04 15:21:11 -0300

    coloquei pop ups

--- demo4.js
+++ demo4.js
@@ -36,25 +36,33 @@ function setup() {
 var buscarImg = function (linkDaImg) {
 
   abrePopUp();
+  abreOriginal();
 
   stack = undefined;
   sorted = false;
   var img = new Image();
+  var original = new Image();
   img.crossOrigin = "Anonymous";
+  original.crossOrigin = "Anonymous";
+
   img.src = linkDaImg;
+  original.src = linkDaImg;
 
   var canvas = document.getElementById("canvas");
   canvas.width = 400;
   canvas.height = 400;
 
+  var canvasOriginal = document.getElementById("original");
+  canvasOriginal.width = 400;
+  canvasOriginal.height = 400;
+
   window.ctx = canvas.getContext("2d");
-
-
-
-
+  
+  var contexto = canvasOriginal.getContext("2d");
 
   img.onload = function () {
 
+    contexto.drawImage(original,0,0);
     ctx.clearRect(0, 0, 400, 400);
     ctx.drawImage(img, 0, 0);
     window.imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -210,6 +218,76 @@ function dragElement(elmnt) {
   if (document.getElementById(elmnt.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+//POPUP IMAGEM ORIGINAL
+// Get the modal
+var modalOriginalPU = document.getElementById("Original");
+
+// Get the <span> element that closes the modal
+var fechar = document.getElementsByClassName("fecharOriginal")[0];
+
+// When the user clicks the button, open the modal 
+var abreOriginal = function() {
+  modalOriginalPU.style.display = "block";
+};
+
+// // When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modalOriginalPU.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalOriginalPU) {
+    modalOriginalPU.style.display = "none";
+  }
+}
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("Original"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
